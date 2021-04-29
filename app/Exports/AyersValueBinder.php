e1aebe67ec2f5faea546a56779633abbe30121b7
GenChasing <dont.be.an.evil@gmail.com>
2021-04-29 13:54:42 +0800

    update

---
+++ app/Exports/AyersValueBinder.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\WithCustomValueBinder;
+use PhpOffice\PhpSpreadsheet\Cell\Cell;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+
+class AyersValueBinder extends DefaultValueBinder implements WithCustomValueBinder
+{
+    public function bindValue(Cell $cell, $value)
+    {
+        if (is_numeric($value)) {
+            $cell->setValueExplicit($value, DataType::TYPE_STRING);
+
+            return true;
+        }
+        // else return default behavior
+        return parent::bindValue($cell, $value);
+    }
+}
--- app/Traits/Excel.php
+++ app/Traits/Excel.php
@@ -1,28 +1,30 @@
 <?php
 namespace App\Traits;
 
-use App\ViewAyersImportData;
+use App\Exports\AyersDataExport;
+use Excel as _Excel;
 use Storage;
 
 trait Excel
 {
     public function exportAyersImportData(array $clients)
     {
-        $Headers = ViewAyersImportData::first()->getAttributes();
-        $passport_ids = [];
-        foreach ($clients as $client) {
-            $passport_ids[] = $client['證件號碼'];
-        }
-        $ViewAyersImportData = ViewAyersImportData::whereIn('passport_id', $passport_ids)->get();
-        $html = view('excel.AyersImportData', [
-            'Headers' => $Headers,
-            'AyersImportData' => $ViewAyersImportData,
-        ])->render();
-        $reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
-        $spreadsheet = $reader->loadFromString($html);
-        $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
-        $path = storage_path('app/public/AyersImportData.xlsx');
-        $writer->save($path);
-        return Storage::disk('public')->download('AyersImportData.xlsx');
+        return _Excel::download(new AyersDataExport($clients), 'AyersImportData.xlsx');
+        // $Headers = ViewAyersImportData::first()->getAttributes();
+        // $passport_ids = [];
+        // foreach ($clients as $client) {
+        //     $passport_ids[] = $client['證件號碼'];
+        // }
+        // $ViewAyersImportData = ViewAyersImportData::whereIn('passport_id', $passport_ids)->get();
+        // $html = view('excel.AyersImportData', [
+        //     'Headers' => $Headers,
+        //     'AyersImportData' => $ViewAyersImportData,
+        // ])->render();
+        // $reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+        // $spreadsheet = $reader->loadFromString($html);
+        // $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+        // $path = storage_path('app/public/AyersImportData.xlsx');
+        // $writer->save($path);
+        // return Storage::disk('public')->download('AyersImportData.xlsx');
     }
 }
