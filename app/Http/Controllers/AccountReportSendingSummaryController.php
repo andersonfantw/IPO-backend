879c81155621419b8ce43cdd960b7b6c18225779
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-06-01 10:24:57 +0800

    controller

---
+++ app/Http/Controllers/AccountReportSendingSummaryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AccountReportSendingSummaryController extends HomeController
+{
+    protected $name = 'AccountReportSendingSummary';
+}
---
+++ resources/views/pdf/AnnualAccountReport.blade.php
@@ -0,0 +1,15 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="x-ua-compatible" content="IE=9;IE=8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>accountOpeningForm DOWNLOAD</title>
+    <style>
+    </style>    
+</head>
+<body>
+</body>
+</html>
