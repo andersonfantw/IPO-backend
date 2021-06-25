f5044d24bef80daf418f7e766fa55e9d1bc0b8cc
GenChasing <dont.be.an.evil@gmail.com>
2021-06-25 12:00:09 +0800

    update

--- app/Http/Controllers/UnauditedList1Controller.php
+++ app/Http/Controllers/UnauditedList1Controller.php
@@ -63,15 +63,17 @@ class UnauditedList1Controller extends HomeController
         //     $query->where('status', 'unaudited');
         // })->where('status', 'unaudited')->orderBy('created_at', 'desc')->limit(10)->get();
 
+        // $Clients = DB::select('select * from users where active = ?', [1]);
+
         $Clients = Client::has('ClientBusinessType')->whereHas('ClientSignature', function (Builder $query) {
             $query->where('status', 'unaudited');
         })->where('status', 'unaudited')->orderBy('created_at', 'desc')->limit(10)->get();
         $rows = [];
         foreach ($Clients as $Client) {
             $row = [];
-            $row['客户姓名'] = $Client->ViewClientIDCard->name_c;
-            $row['證件號碼'] = $Client->ViewClientIDCard->idcard_no;
-            $row['所在地'] = $Client->ViewClientIDCard->address;
+            $row['客户姓名'] = $Client->IDCard->name_c;
+            $row['證件號碼'] = $Client->IDCard->idcard_no;
+            $row['所在地'] = $Client->IDCard->address;
             $row['手機號碼'] = $Client->mobile;
             $row['郵箱'] = $Client->email;
             $row['提交時間'] = date_format($Client->created_at, "Y-m-d H:i:s");
