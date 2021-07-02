bf2ec2b20fbfb13055fbbc0610242a834f0797c6
GenChasing <dont.be.an.evil@gmail.com>
2021-07-02 10:26:59 +0800

    update

--- resources/js/components/UnauditedList2.vue
+++ resources/js/components/UnauditedList2.vue
@@ -113,7 +113,7 @@ export default {
       data: null,
       selectedClients: null,
       currentPage: 1,
-      perPage: 10,
+      perPage: 20,
       FilterType: {},
       totalRows: 0,
       aes: [
