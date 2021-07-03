090aab12420e09bbc7d24943a2c9dddd40429833
GenChasing <dont.be.an.evil@gmail.com>
2021-07-03 14:26:05 +0800

    update

--- resources/views/ViewClientBankCard.blade.php
+++ resources/views/ViewClientBankCard.blade.php
@@ -4,5 +4,5 @@
     <h1 class="text-warning text-center">已審核添加銀行卡申請</h1>
     <view-client-bank-card :client="'{{ $Client }}'" :client_bank_card="'{{ $ClientBankCard }}'"
         :client_id_card="'{{ $ClientIDCard }}'"
-        :bank_card_image="'{{ route('LoadBankcard', ['id' => $ClientBankCardID]) }}'" />
+        :bank_card_image="'{{ route('LoadBankCard', ['id' => $ClientBankCardID]) }}'" />
 @endsection
--- resources/views/ViewClientFundInRequest.blade.php
+++ resources/views/ViewClientFundInRequest.blade.php
@@ -5,5 +5,5 @@
     <view-client-fund-in-request :request="'{{ $Request }}'" :client="'{{ $Client }}'"
         :client_id_card="'{{ $ClientIDCard }}'" :ayers_accounts="{{ $AyersAccounts }}"
         :receipt="'{{ route('LoadReceipt', ['id' => $Request_ID]) }}'"
-        :bank_card="'{{ route('LoadBankcard', ['id' => $Request_ID]) }}'" />
+        :bank_card="'{{ route('LoadBankCard', ['id' => $Request_ID]) }}'" />
 @endsection
