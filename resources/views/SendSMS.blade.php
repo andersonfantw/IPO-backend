df340d7e41a53ea1daf8a785c6be0fa23ce9c349
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-07-08 16:33:04 +0800

    fix

--- resources/views/SendSMS.blade.php
+++ resources/views/SendSMS.blade.php
@@ -4,8 +4,12 @@
     <body>
         <form method="post">
             @csrf
+            <h1>簡訊發送工具</h1>
+            <h5>顯示名稱</h5>
             <input id="senderid" name="senderid" type="text" value="CYSS" />
+            <h5>接收人電話 (以逗號分隔)</h5>
             <textarea id="recipient" rows="10" name="recipient" type="text"></textarea>
+            <h5>簡訊內容</h5>
             <textarea id="content" rows="10" name="content" type="text" value=""></textarea>
             <button onclick="sendsms()">發送簡訊</button>
         </form>
