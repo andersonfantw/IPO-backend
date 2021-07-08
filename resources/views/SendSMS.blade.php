6c04a5b231c05582199a9498edcc3e33d67a711d
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-07-08 16:27:48 +0800

    fix

--- resources/views/SendSMS.blade.php
+++ resources/views/SendSMS.blade.php
@@ -4,9 +4,9 @@
     <body>
         <form method="post">
             @csrf
-            <input id="senderid" type="text" value="CYSS" />
-            <input id="recipient" type="text" value="" />
-            <input id="content" type="text" value="" />
+            <input id="senderid" name="senderid" type="text" value="CYSS" />
+            <input id="recipient" name="recipient" type="text" value="" />
+            <input id="content" name="content" type="text" value="" />
             <button onclick="sendsms()">發送簡訊</button>
         </form>
     </body>
