91381727caaf32ff020534606a14a2d182266b26
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-07-08 15:28:19 +0800

    fix

--- config/notification.php
+++ config/notification.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'Meteotsis' => [
+    'Meteorsis' => [
         'username' => env('HK_SMS_USERNAME'),
         'password' => env('HK_SMS_PASSWORD'),
         'url' => env('HK_SMS_URL'),
