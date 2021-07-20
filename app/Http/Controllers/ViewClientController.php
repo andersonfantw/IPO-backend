d5d85819f2e2257eb761b84b168006345c0d51df
GenChasing <dont.be.an.evil@gmail.com>
2021-07-20 15:38:14 +0800

    update

--- app/Http/Controllers/ViewClientController.php
+++ app/Http/Controllers/ViewClientController.php
@@ -89,7 +89,7 @@ class ViewClientController extends HomeController
             'ClientWorkingStatus',
             'ClientFinancialStatus',
             'ClientInvestmentExperience',
-            'ViewClientScore',
+            'ViewClientQuestionnaire',
             'ClientEvaluationResults',
             'ClientSignature',
             'ClientBusinessType',
@@ -105,7 +105,7 @@ class ViewClientController extends HomeController
             $Client->ClientAddressProof->image = null;
             $Client->ClientAddressProof->detailed_address = addslashes($Client->ClientAddressProof->detailed_address);
             $details['ClientAddressProof'] = $Client->ClientAddressProof->toJson(JSON_UNESCAPED_UNICODE);
-            $Client->ClientAddressProof->detailed_address = stripslashes($Client->ClientAddressProof->detailed_address);
+            // $Client->ClientAddressProof->detailed_address = stripslashes($Client->ClientAddressProof->detailed_address);
         } else {
             $details['ClientAddressProof'] = null;
         }
@@ -120,7 +120,6 @@ class ViewClientController extends HomeController
         $Client->ClientFinancialStatus->fund_source = addslashes($Client->ClientFinancialStatus->fund_source);
         $details['ClientFinancialStatus'] = $Client->ClientFinancialStatus->toJson(JSON_UNESCAPED_UNICODE);
         $details['ClientInvestmentExperience'] = $Client->ClientInvestmentExperience->toJson(JSON_UNESCAPED_UNICODE);
-        // $details['ClientScore'] = $Client->ViewClientScore->toJson(JSON_UNESCAPED_UNICODE);
         $details['ClientScore'] = json_encode($this->calculateClientScore($Client), JSON_UNESCAPED_UNICODE);
         $details['ClientEvaluationResults'] = $Client->ClientEvaluationResults->toJson(JSON_UNESCAPED_UNICODE);
         $details['ClientSignature'] = $Client->ClientSignature->toJson(JSON_UNESCAPED_UNICODE);
@@ -133,66 +132,21 @@ class ViewClientController extends HomeController
             $details['ClientDepositProof'] = null;
         }
         $details['Introducer'] = $Client->ViewIntroducer->toJson(JSON_UNESCAPED_UNICODE);
+        $Client = Client::where('uuid', $request->input('uuid'))->first();
         $Client->idcard_type = addslashes($Client->idcard_type);
         $Client->education_level = addslashes($Client->education_level);
         $Client->selected_flow = addslashes($Client->selected_flow);
         $details['Client'] = $Client->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->idcard_type = stripslashes($Client->idcard_type);
+        // $Client->idcard_type = stripslashes($Client->idcard_type);
         return $details;
     }
 
     protected function setViewParameters(Request $request)
     {
         $parameters = parent::setViewParameters($request);
-        $input = $request->all();
-        $Client = Client::where('uuid', $input['uuid'])->first();
-        $Client->idcard_type = addslashes($Client->idcard_type);
-        $Client->education_level = addslashes($Client->education_level);
-        $Client->selected_flow = addslashes($Client->selected_flow);
-        $parameters['Client'] = $Client->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->idcard_type = stripslashes($Client->idcard_type);
-        $Client->education_level = stripslashes($Client->education_level);
-        $Client->selected_flow = stripslashes($Client->selected_flow);
-        $parameters['uuid'] = $Client->uuid;
-        $parameters['redirect_route'] = $input['redirect_route'];
-        $Client->IDCard->idcard_face = null;
-        $Client->IDCard->idcard_back = null;
-        $parameters['ClientIDCard'] = $Client->IDCard->toJson(JSON_UNESCAPED_UNICODE);
-        if (is_object($Client->ClientAddressProof)) {
-            $Client->ClientAddressProof->image = null;
-            $Client->ClientAddressProof->detailed_address = addslashes($Client->ClientAddressProof->detailed_address);
-            $parameters['ClientAddressProof'] = $Client->ClientAddressProof->toJson(JSON_UNESCAPED_UNICODE);
-            $Client->ClientAddressProof->detailed_address = stripslashes($Client->ClientAddressProof->detailed_address);
-        } else {
-            $parameters['ClientAddressProof'] = null;
-        }
-        foreach ($Client->ClientBankCards as &$ClientBankCard) {
-            $ClientBankCard->bankcard_blob = null;
-        }
-        $parameters['ClientBankCards'] = $Client->ClientBankCards->toJson(JSON_UNESCAPED_UNICODE);
-        if ($Client->ClientWorkingStatus->name_card_face) {
-            $Client->ClientWorkingStatus->name_card_face = null;
-        }
-        $parameters['ClientWorkingStatus'] = $Client->ClientWorkingStatus->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->ClientFinancialStatus->fund_source = addslashes($Client->ClientFinancialStatus->fund_source);
-        $parameters['ClientFinancialStatus'] = $Client->ClientFinancialStatus->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->ClientFinancialStatus->fund_source = stripslashes($Client->ClientFinancialStatus->fund_source);
-        $parameters['ClientInvestmentExperience'] = $Client->ClientInvestmentExperience->toJson(JSON_UNESCAPED_UNICODE);
-        $parameters['ClientScore'] = $Client->ViewClientScore->toJson(JSON_UNESCAPED_UNICODE);
-        // $parameters['ClientScore'] = '[]';
-        $parameters['ClientEvaluationResults'] = $Client->ClientEvaluationResults->toJson(JSON_UNESCAPED_UNICODE);
-        $parameters['ClientSignature'] = $Client->ClientSignature->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->ClientBusinessType->direct_promotion = addslashes($Client->ClientBusinessType->direct_promotion);
-        $parameters['ClientBusinessType'] = $Client->ClientBusinessType->toJson(JSON_UNESCAPED_UNICODE);
-        $Client->ClientBusinessType->direct_promotion = stripslashes($Client->ClientBusinessType->direct_promotion);
-        if (is_object($Client->ClientDepositProof)) {
-            $Client->ClientDepositProof->image = null;
-            $parameters['ClientDepositProof'] = $Client->ClientDepositProof->toJson(JSON_UNESCAPED_UNICODE);
-        } else {
-            $parameters['ClientDepositProof'] = null;
-        }
-        $parameters['Introducer'] = $Client->ViewIntroducer->toJson(JSON_UNESCAPED_UNICODE);
-        return $parameters;
+        $parameters['redirect_route'] = $request->input('redirect_route');
+        $details = $this->getClientDetails($request);
+        return array_merge($parameters, $details);
     }
 
 }
