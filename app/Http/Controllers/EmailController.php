a656d13f5704d583d2f1a56bd20d8487bff3e35d
GenChasing <dont.be.an.evil@gmail.com>
2021-07-29 14:16:49 +0800

    update

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App;
 use App\Client;
-use App\Mail\AccountOpened;
+use App\Services\NotifyMessage;
+use App\Services\NotifyService;
 use App\Traits\Score;
 use Illuminate\Http\Request;
-use Mail;
 
 class EmailController extends Controller
 {
@@ -19,9 +19,26 @@ class EmailController extends Controller
         $clients = $request->input('clients');
         $User = $request->input('User');
         foreach ($clients as $client) {
-            $Client = Client::where('uuid', $client['uuid'])->first();
-            // App::setLocale($Client->nationality);
-            Mail::to($Client->email, $Client->IDCard->name_c)->send(new AccountOpened($Client, $User));
+            $Client = Client::with(['IDCard', 'AyersAccounts'])->where('uuid', $client['uuid'])->first();
+            $account_no = $Client->AyersAccounts->first()->account_no;
+            // $client_id = substr($account_no, 0, -2);
+            $client_id = '200001';
+            // Mail::to($Client->email, $Client->IDCard->name_c)->send(new AccountOpened($Client, $User));
+            $account_no = [];
+            $account_type = [];
+            foreach ($Client->AyersAccounts as $AyersAccount) {
+                $account_no[] = $AyersAccount->account_no;
+                $account_type[] = $AyersAccount->type;
+            }
+            $score = $Client->ViewClientScore->sum('score');
+            $params = [
+                'account_name' => $Client->IDCard->name_c,
+                'account_no' => implode(", ", $account_no),
+                'account_type' => implode(", ", $account_type),
+                'level' => $this->getLevel($score),
+                'risk_tolerance' => $this->風險承受程度($score),
+            ];
+            (new NotifyService)->notify((new NotifyMessage)->clientId($client_id)->params($params)->templateId(7)->title('帳戶開戶通知書'));
         }
     }
 }
