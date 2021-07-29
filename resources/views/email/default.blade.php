c1b1c6e86798eb7fb0054719ef7e9a0fafacff76
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-07-29 17:19:44 +0800

    fix

---
+++ resources/views/email/default.blade.php
@@ -0,0 +1,12 @@
+<p>{{ $greeting }}</p>
+<p>{{ $content }}</p>
+<br />
+<span class="s11">Best Regards,</span><br />
+<br />
+<b class="zh s12">中國銀盛國際證券有限公司</b><br />
+<b class="s12">CHINA YINSHENG INTERNATIONAL SECURITIES LIMITED</b><br />
+<span class="zh s11">香港上環德輔道西9號6樓</span><br />
+<small>6/F., 9 Des Voeux Road West, Sheung Wan, Hong Kong.</small><br />
+<small>Tel. (852) 2626-0363    Fax. (852) 2111-1052</small><br />
+<br />
+© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
