440af5e5d49b030b4b4467a2b4545872754e7c9c
GenChasing <dont.be.an.evil@gmail.com>
2021-07-29 17:45:06 +0800

    update

--- app/Http/Controllers/ViewClientFundInRequestController.php
+++ app/Http/Controllers/ViewClientFundInRequestController.php
@@ -25,44 +25,49 @@ class ViewClientFundInRequestController extends HomeController
     {
         $parameters = parent::setViewParameters($request);
         $input = $request->all();
-        $ClientFundInRequest = ClientFundInRequest::find($input['id']);
+        $ClientFundInRequest = ClientFundInRequest::with(['Client', 'Client.ViewClientIDCard', 'Client.AyersAccounts'])
+            ->find($input['id']);
         if (is_object($ClientFundInRequest)) {
             $ClientFundInRequest->receipt = null;
             $ClientFundInRequest->bankcard = null;
+            $client_fund_in_request = [];
             foreach ($ClientFundInRequest->getAttributes() as $key => $value) {
-                $ClientFundInRequest->{$key} = addslashes($value);
+                // $ClientFundInRequest->{$key} = addslashes($value);
+                $client_fund_in_request[$key] = addslashes($value);
             }
-        }
-        $parameters['Request_ID'] = $ClientFundInRequest->id;
-        $parameters['Request'] = $ClientFundInRequest->toJson(JSON_UNESCAPED_UNICODE);
+            $parameters['Request_ID'] = $client_fund_in_request['id'];
+            $parameters['Request'] = json_encode($client_fund_in_request, JSON_UNESCAPED_UNICODE);
 
-        $Client = $ClientFundInRequest->Client;
-        if (is_object($Client)) {
-            foreach ($Client->getAttributes() as $key => $value) {
-                $Client->{$key} = addslashes($value);
-            }
-        }
-        $parameters['Client'] = $Client->toJson(JSON_UNESCAPED_UNICODE);
+            $Client = $ClientFundInRequest->Client;
+            if (is_object($Client)) {
+                $client = [];
+                foreach ($Client->getAttributes() as $key => $value) {
+                    // $Client->{$key} = addslashes($value);
+                    $client[$key] = addslashes($value);
+                }
+                $parameters['Client'] = json_encode($client, JSON_UNESCAPED_UNICODE);
 
-        $ClientIDCard = $Client->ViewClientIDCard;
-        if (is_object($ClientIDCard)) {
-            $ClientIDCard->idcard_face = null;
-            $ClientIDCard->idcard_back = null;
-            foreach ($ClientIDCard->getAttributes() as $key => $value) {
-                $ClientIDCard->{$key} = addslashes($value);
-            }
-        }
-        $parameters['ClientIDCard'] = $ClientIDCard->toJson(JSON_UNESCAPED_UNICODE);
+                $ClientIDCard = $Client->ViewClientIDCard;
+                if (is_object($ClientIDCard)) {
+                    $ClientIDCard->idcard_face = null;
+                    $ClientIDCard->idcard_back = null;
+                    foreach ($ClientIDCard->getAttributes() as $key => $value) {
+                        $ClientIDCard->{$key} = addslashes($value);
+                    }
+                }
+                $parameters['ClientIDCard'] = $ClientIDCard->toJson(JSON_UNESCAPED_UNICODE);
 
-        $AyersAccounts = [];
-        foreach ($Client->AyersAccounts as $AyersAccount) {
-            $AyersAccount = $AyersAccount->attributesToArray();
-            foreach ($AyersAccount as $key => $value) {
-                $AyersAccount[$key] = addslashes($value);
+                $AyersAccounts = [];
+                foreach ($Client->AyersAccounts as $AyersAccount) {
+                    $AyersAccount = $AyersAccount->attributesToArray();
+                    foreach ($AyersAccount as $key => $value) {
+                        $AyersAccount[$key] = addslashes($value);
+                    }
+                    $AyersAccounts[] = $AyersAccount;
+                }
+                $parameters['AyersAccounts'] = json_encode($AyersAccounts, JSON_UNESCAPED_UNICODE);
             }
-            $AyersAccounts[] = $AyersAccount;
         }
-        $parameters['AyersAccounts'] = json_encode($AyersAccounts, JSON_UNESCAPED_UNICODE);
         return $parameters;
     }
 }
