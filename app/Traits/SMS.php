669072b4644d96636230b2f28aa89776bbb1da0d
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-08-13 10:20:29 +0800

    fix

--- app/Traits/SMS.php
+++ app/Traits/SMS.php
@@ -72,28 +72,7 @@ trait SMS
         $recipient = $Client->mobile;
         $country_code = config("locale.CountryCode.$Client->nationality");
         $introducer_uuid = $Client->introducer_uuid;
-
-        $contents = <<<TEXT
-【銀盛證券】尊敬的客戶，您開戶未能通過審核，請于5日内點擊以下鏈接(https://pys.chinayss.hk)登入開戶界面，並根據提示更新相應資料。感謝使用銀盛證券服務！
-TEXT;
-
-        (new NotifyService)->notify((new NotifyMessage)->mobile("$country_code$recipient")->content($contents));
-
-        // $contents = [
-        //     '[中國銀盛國際證券]',
-        //     "閣下所提交的證券戶口開戶資料未能通過審核，請重新掃瞄二維碼（QR Code）或點擊以下開戶連結（https://pys.chinayss.hk）進入開戶流程，根據開戶流程中的指示修正或更新相應資料，所有資料修正完畢後頁面會跳到最後一個步驟，代表開戶資料已成功重新提交。謝謝您的耐心配合。",
-        // ];
-        // $content = implode("\n", $contents);
-        // $response = Http::get(env('METEORSIS_API_URL'), [
-        //     'langeng' => 0,
-        //     'dos' => 'now',
-        //     'senderid' => 'CYSS',
-        //     'content' => $this->Text2Unicode($content),
-        //     'recipient' => "$country_code$recipient",
-        //     'username' => env('METEORSIS_ACCESS_KEY_ID'),
-        //     'password' => env('METEORSIS_SECRET_ACCESS_KEY'),
-        // ]);
-        // return $response;
+        (new NotifyService)->notify((new NotifyMessage)->mobile("$country_code$recipient")->templateId(8)->params(['uuid'=>$row->introducer_uuid]));
     }
 
     public function NoticeClientCorrectToRejectItemOn5days(){
@@ -105,9 +84,7 @@ TEXT;
             ->selectRaw('concat(client.country_code,client.mobile) as mobile')
             ->selectRaw('count(*) as Rejected')->get();
         foreach($EditableSteps as $row){
-            dd((new NotifyMessage)->mobile($row->mobile)->templateId(9)->toData());
-            // (new NotifyService)->notify((new NotifyMessage)->mobile($row->mobile)->templateId(9)->params(['uuid'=>$row->introducer_uuid]));
-            (new NotifyService)->notify((new NotifyMessage)->mobile('85255984928')->templateId(9)->params(['uuid'=>$row->introducer_uuid]));
+            (new NotifyService)->notify((new NotifyMessage)->mobile($row->mobile)->templateId(9)->params(['uuid'=>$row->introducer_uuid]));
         }
     }
 
