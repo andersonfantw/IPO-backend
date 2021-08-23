eb03a8d31d5d550afa90934484f058342da17a33
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-08-23 11:38:25 +0800

    fix

--- app/Http/Controllers/AeCommissionSummaryController.php
+++ app/Http/Controllers/AeCommissionSummaryController.php
@@ -55,7 +55,7 @@ class AeCommissionSummaryController extends HomeController
                 $v['name']='王浩進';
                 $v['codes'] = $v['codes'].',AEWHC';
             }
-            foreach(AeCommissionSummary::where('ae_codes','=',$v['codes'])->where('buss_date',$start_date)->get()->toArray() as $r) $hash[$r->cate] = collect($r)->toArray();
+            foreach(AeCommissionSummary::where('ae_codes','=',$v['codes'])->where('buss_date','=',$start_date)->get()->toArray() as $r) $hash[$r['cate']] = collect($r)->toArray();
             $arr1 = array(
                 'id' => 0,
                 'name' => $v['name'],
@@ -191,7 +191,7 @@ class AeCommissionSummaryController extends HomeController
         }
 
         $hash = $arr;
-        foreach(AeCommissionSummary::where('ae_codes','=',$AE['codes'])->where('buss_date',$start_date)->get()->toArray() as $r) $hash[$r->cate] = collect($r)->toArray();
+        foreach(AeCommissionSummary::where('ae_codes','=',$AE['codes'])->where('buss_date','=',$start_date)->get()->toArray() as $r) $hash[$r->cate] = collect($r)->toArray();
         $result = array_merge([
             'id' => 0,
             'name' => $AE['name'],
