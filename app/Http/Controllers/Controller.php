e132626459b82e6bbe3f149ce1bd26c17e268a63
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-08-24 16:01:13 +0800

    fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use App\Services\NotifyMessage;
+use App\Services\NotifyService;
 
 class Controller extends BaseController
 {
@@ -26,5 +28,38 @@ class Controller extends BaseController
     {
         $route = $request->input('route');
         dd(config($route));
+    }
+
+    public function testSendMsg(){
+        $contents =<<<TEXT
+[中國銀盛國際證券]
+閣下所提交的證券戶口開戶資料未能通過審核，請重新掃瞄二維碼（QR Code）或點擊以下開戶連結（https://pys.chinayss.hk）進入開戶流程，根據開戶流程中的指示修正或更新相應資料，所有資料修正完畢後頁面會跳到最後一個步驟，代表開戶資料已成功重新提交。謝謝您的耐心配合。
+TEXT;
+        (new NotifyService)->notify(
+            (new NotifyMessage)->mobile('85255984928')->content($contents)
+        );
+        return ['ok'=>true];
+    }
+
+    public function testSendEmail(){
+        $contents =<<<TEXT
+[中國銀盛國際證券]
+閣下所提交的證券戶口開戶資料未能通過審核，請重新掃瞄二維碼（QR Code）或點擊以下開戶連結（https://pys.chinayss.hk）進入開戶流程，根據開戶流程中的指示修正或更新相應資料，所有資料修正完畢後頁面會跳到最後一個步驟，代表開戶資料已成功重新提交。謝謝您的耐心配合。
+TEXT;
+        (new NotifyService)->notify(
+            (new NotifyMessage)->email('andersonfantw@gmail.com')->title('證券戶口開戶資料未能通過審核')->content($contents)
+        );
+        return ['ok'=>true];
+    }
+
+    public function testSendAccountOverview(){
+        $contents =<<<TEXT
+[中國銀盛國際證券]
+閣下所提交的證券戶口開戶資料未能通過審核，請重新掃瞄二維碼（QR Code）或點擊以下開戶連結（https://pys.chinayss.hk）進入開戶流程，根據開戶流程中的指示修正或更新相應資料，所有資料修正完畢後頁面會跳到最後一個步驟，代表開戶資料已成功重新提交。謝謝您的耐心配合。
+TEXT;
+        (new NotifyService)->notify(
+            (new NotifyMessage)->clientId('200001')->title('證券戶口開戶資料未能通過審核')->content($contents)
+        );
+        return ['ok'=>true];
     }
 }
