8c40f5e3daa3002233b89ca5d8b873ed6fa1a5bd
GenChasing <dont.be.an.evil@gmail.com>
2021-09-06 17:50:20 +0800

    update

--- app/Http/Controllers/CheckingDepositController.php
+++ app/Http/Controllers/CheckingDepositController.php
@@ -101,7 +101,7 @@ class CheckingDepositController extends Controller
         $now = Carbon::now()->toDateTimeString();
         // UnknownDeposit::where('uploaded_at', $today)->delete();
         // (new UnknownDepositsImport)->import($request->file('file')->path(), null, \Maatwebsite\Excel\Excel::XLSX);
-        _Excel::import(new UnknownDepositsImport($now), $request->file('file')->path());
+        _Excel::import(new UnknownDepositsImport($now), $request->file('file'));
         $UnknownDeposits = UnknownDeposit::has('ClientDepositIdentificationCode')->where('uploaded_at', $now)->update(['status' => 'matched']);
     }
 
