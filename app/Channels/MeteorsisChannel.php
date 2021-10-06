bc3f52470ee3693d316b9bcf5a6a29c3febeec7f
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-10-06 13:15:47 +0800

    fix

--- app/Channels/MeteorsisChannel.php
+++ app/Channels/MeteorsisChannel.php
@@ -4,7 +4,7 @@ namespace App\Channels;
 use Cuby\Meteorsis\MeteorsisService;
 use Illuminate\Notifications\Notification;
 use App\Notifications\MeteorsisSMS;
-use App\NotificationRecord;
+use App\Models\NotificationRecord;
 use Carbon\Carbon;
 
 class MeteorsisChannel
