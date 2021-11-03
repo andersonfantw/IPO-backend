89c58149d9db8eb2613ac7e74b8f56cfe01093f4
ANDERSONFAN-TPC\AndersonFan <andersonfan@chinayss.hk>
2021-11-03 15:51:47 +0800

    fix

--- app/Http/Controllers/AeCommissionSummaryController.php
+++ app/Http/Controllers/AeCommissionSummaryController.php
@@ -319,6 +319,7 @@ class AeCommissionSummaryController extends HomeController
                 'reservations'=>$collect->sum('reservations'),
                 'commission'=>$collect->sum('commission'),
                 'performance'=>$collect->sum('performance'),
+                'cost'=>$collect->sum('cost'),
                 'total_group_open'=>$group['total_group_open']??null,
                 'total_group_commission'=>$group['total_group_commission']??null,
             ],
